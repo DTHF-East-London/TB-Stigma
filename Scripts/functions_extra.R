ce0c45eccc33237039b1096d7c8c6e9027eb5b0a
Freedom Mukomana <Freedomm@foundation.co.za>
2023-08-04 11:26:01 +0200

    minor changes

--- Scripts/functions_extra.R
+++ Scripts/functions_extra.R
@@ -1,6 +1,5 @@
 getTBExtractionRecords <- function(dataset){
   temp <- dataset[c(1,58,65:68,752:780,781:793)]
-  View(temp)
-  temp <- subset(temp, hhc_pt_days_to_present>30)
+  temp <- subset(temp, !is.na(temp$hhc_pc_days_since_referral) & temp$hhc_pc_days_since_referral>30)
   return(temp)
 }
