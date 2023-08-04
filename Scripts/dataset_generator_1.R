c62e35cb78f6005b1daeb2f0d939049314c24bd6
Freedom Mukomana <Freedomm@foundation.co.za>
2023-08-04 11:26:29 +0200

    Drop record 53

--- Scripts/dataset_generator_1.R
+++ Scripts/dataset_generator_1.R
@@ -49,6 +49,12 @@ for(event in events){
   
 }
 
+#Drop record 53
+
+raw_data_baseline_arm_1 <- subset(raw_data_baseline_arm_1, record_id!='53')
+raw_data_hhci_visit_info_arm_1 <- subset(raw_data_hhci_visit_info_arm_1, record_id!='53')
+raw_data_hhci_info_arm_1 <- subset(raw_data_hhci_info_arm_1, record_id!='53')
+
 #Adding additional calculated variables
 #raw_data_hhci_info_arm_1$hhc_
 
@@ -63,13 +69,64 @@ raw_data_hhci_info_arm_1$hhc_sc_clinic_visit[!is.na(raw_data_hhci_info_arm_1$hhc
 levels(raw_data_hhci_info_arm_1$hhc_sc_provide_sputum) <- c('No', 'Yes', 'NI')
 raw_data_hhci_info_arm_1$hhc_sc_provide_sputum[!is.na(raw_data_hhci_info_arm_1$hhc_sc_verbal_consent)] <- 'NI'
 
-raw_data_baseline_arm_1$tbip_sc_eligible[raw_data_baseline_arm_1$record_id == '145'] <- 'Proceed'
+#raw_data_baseline_arm_1$tbip_sc_eligible[raw_data_baseline_arm_1$record_id == '145'] <- 'Proceed'
 
 raw_data_hhci_info_arm_1 <- raw_data_hhci_info_arm_1 %>% mutate(hhc_days_since_referral = difftime(today, as.POSIXct(as.Date(hhc_sc_date_cons, format = '%Y-%m-%d')), units = 'days')) %>% relocate(hhc_days_since_referral, .after = 'hhc_sc_date_cons')
 raw_data_hhci_info_arm_1 <- raw_data_hhci_info_arm_1 %>% mutate(hhc_pc_days_to_present = difftime(as.POSIXct(as.Date(hhc_pc_presentation_date, format = '%Y-%m-%d')), as.POSIXct(as.Date(hhc_sc_date_cons, format = '%Y-%m-%d')), units = 'days')) %>% relocate(hhc_pc_days_to_present, .after = 'hhc_sc_date_cons')
 raw_data_hhci_info_arm_1 <- raw_data_hhci_info_arm_1 %>% mutate(hhc_pt_days_to_present = difftime(as.POSIXct(as.Date(hhc_pt_return_date, format = '%Y-%m-%d')), as.POSIXct(as.Date(hhc_sc_date_cons, format = '%Y-%m-%d')), units = 'days')) %>% relocate(hhc_pt_days_to_present, .after = 'hhc_pc_days_to_present')
 raw_data_hhci_info_arm_1 <- raw_data_hhci_info_arm_1 %>% mutate(hhc_pt_days_to_present = difftime(as.POSIXct(as.Date(hhc_pt_return_date, format = '%Y-%m-%d')), as.POSIXct(as.Date(hhc_sc_date_cons, format = '%Y-%m-%d')), units = 'days')) %>% relocate(hhc_pt_days_to_present, .after = 'hhc_sc_date_cons')
 
+ip_enrollment <- raw_data_baseline_arm_1[c(1,7)]
+
+raw_data_hhci_info_arm_1 <- left_join(raw_data_hhci_info_arm_1, ip_enrollment) %>% relocate(tbip_sc_date, .after = record_id)
+
+raw_data_hhci_info_arm_1 <- raw_data_hhci_info_arm_1 %>% mutate(hhc_collection_point = case_when(as.Date(tbip_sc_date) == as.Date(hhcl_date) ~ "Clinic", as.Date(tbip_sc_date) != as.Date(hhcl_date) ~ "HH")) %>% relocate(hhc_collection_point, .after = tbip_sc_date)
+
+#Visit 1
+visit_attempt <- as.data.frame.table(table(raw_data_hhci_info_arm_1$record_id, raw_data_hhci_info_arm_1$hhc_sc_visit_attempt___1))
+
+temp_1 <- subset(visit_attempt, Var2=="Checked")
+temp_1 <- temp_1[c(1,3)]
+names(temp_1) <- c("record_id", "hhc_members_visited_1")
+
+temp_2 <- subset(visit_attempt, Var2=="Unchecked")
+temp_2 <- temp_2[c(1,3)]
+names(temp_2) <- c("record_id", "hhc_members_not_visited_1")
+
+visit_attempt <- left_join(temp_1, temp_2)
+
+raw_data_baseline_arm_1 <- left_join(raw_data_baseline_arm_1, visit_attempt)
+
+#Visit 2
+visit_attempt <- as.data.frame.table(table(raw_data_hhci_info_arm_1$record_id, raw_data_hhci_info_arm_1$hhc_sc_visit_attempt___2))
+
+temp_1 <- subset(visit_attempt, Var2=="Checked")
+temp_1 <- temp_1[c(1,3)]
+names(temp_1) <- c("record_id", "hhc_members_visited_2")
+
+temp_2 <- subset(visit_attempt, Var2=="Unchecked")
+temp_2 <- temp_2[c(1,3)]
+names(temp_2) <- c("record_id", "hhc_members_not_visited_2")
+
+visit_attempt <- left_join(temp_1, temp_2)
+
+raw_data_baseline_arm_1 <- left_join(raw_data_baseline_arm_1, visit_attempt)
+
+
+#Visit 3
+visit_attempt <- as.data.frame.table(table(raw_data_hhci_info_arm_1$record_id, raw_data_hhci_info_arm_1$hhc_sc_visit_attempt___3))
+
+temp_1 <- subset(visit_attempt, Var2=="Checked")
+temp_1 <- temp_1[c(1,3)]
+names(temp_1) <- c("record_id", "hhc_members_visited_3")
+
+temp_2 <- subset(visit_attempt, Var2=="Unchecked")
+temp_2 <- temp_2[c(1,3)]
+names(temp_2) <- c("record_id", "hhc_members_not_visited_3")
+
+visit_attempt <- left_join(temp_1, temp_2)
+
+raw_data_baseline_arm_1 <- left_join(raw_data_baseline_arm_1, visit_attempt)
 #raw_data_baseline_arm_1 <- raw_data_baseline_arm_1 %>% 
 #  select(record_id, contains("tbip_q1_pc_q")) %>% 
 #  rowwise() %>%
