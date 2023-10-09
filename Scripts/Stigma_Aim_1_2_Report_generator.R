c5228e555c82477fc02c96681f82de4ada00e2e8
Freedom Mukomana <Freedomm@foundation.co.za>
2023-10-09 10:55:56 +0200

    Minor changes

--- Scripts/Stigma_Aim_1_2_Report_generator.R
+++ Scripts/Stigma_Aim_1_2_Report_generator.R
@@ -3,7 +3,7 @@ library(xlsx)
 source("Scripts/functions.R")
 
 
-source("Scripts/dataset_generator_1.R")
+#source("Scripts/dataset_generator_1.R")
 
 options(java.parameters = "- Xmx2048m")
 
@@ -431,10 +431,10 @@ setCellValue(cells[["31.4"]], nrow(subset(raw_data_baseline_arm_1, raw_data_base
 setCellValue(cells[["31.6"]], nrow(subset(raw_data_baseline_arm_1, raw_data_baseline_arm_1$tbip_sc_ini_days_calc<14 & raw_data_baseline_arm_1$index_questionnaire_3_complete=='Complete')))
 setCellValue(cells[["31.7"]], nrow(subset(raw_data_baseline_arm_1, raw_data_baseline_arm_1$tbip_sc_ini_days_calc<14 & raw_data_baseline_arm_1$tbr_smear_res_1=='Negative')))
 setCellValue(cells[["31.8"]], nrow(subset(raw_data_baseline_arm_1, raw_data_baseline_arm_1$tbip_sc_ini_days_calc<14 & (raw_data_baseline_arm_1$tbr_smear_res_1=='Positive 1+' | raw_data_baseline_arm_1$tbr_smear_1=='Positive 2+' | raw_data_baseline_arm_1$tbr_smear_res_1=='Positive 3+'))))
-setCellValue(cells[["31.9"]], nrow(subset(raw_data_follow_up_1_arm_1, raw_data_follow_up_1_arm_1$tbr_sputum_collected=='Yes')))
-setCellValue(cells[["31.10"]], nrow(subset(raw_data_follow_up_1_arm_1, raw_data_follow_up_1_arm_1$tbr_smear_res_1=='Negative')))
-setCellValue(cells[["31.11"]], nrow(subset(raw_data_follow_up_1_arm_1, raw_data_follow_up_1_arm_1$tbr_smear_res_1=='Negative')))
-setCellValue(cells[["31.12"]], nrow(subset(raw_data_follow_up_1_arm_1, raw_data_follow_up_1_arm_1$index_follow_up_questionnaire_3_complete=='Unverified' | raw_data_follow_up_1_arm_1$index_follow_up_questionnaire_3_complete=="Complete")))
+setCellValue(cells[["31.9"]], nrow(subset(raw_data_baseline_arm_1, raw_data_baseline_arm_1$tbip_sc_ini_days_calc<14 & raw_data_baseline_arm_1$index_questionnaire_3_complete=='Complete' & raw_data_baseline_arm_1$tbip_sc_days_since_init>=49)))
+setCellValue(cells[["31.11"]], nrow(subset(raw_data_follow_up_1_arm_1, raw_data_follow_up_1_arm_1$tbr_sputum_collected=='Yes')))
+setCellValue(cells[["31.13"]], nrow(subset(raw_data_follow_up_1_arm_1, raw_data_follow_up_1_arm_1$tbr_smear_res_1=='Negative')))
+setCellValue(cells[["31.15"]], nrow(subset(raw_data_follow_up_1_arm_1, raw_data_follow_up_1_arm_1$index_follow_up_questionnaire_3_complete=='Unverified' | raw_data_follow_up_1_arm_1$index_follow_up_questionnaire_3_complete=="Complete")))
 
 
 
